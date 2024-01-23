d12b8b031ed8d1162eb01d26a0fe924bd9ddc177
bene7797 <benedikt.stoeck@web.de>
2024-01-23 12:45:58 +0100

    Rechtschreibfehler behoben und Kontaktdaten ein- und asubelndbar gemacht

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -1,15 +1,31 @@
+import React, { useState } from "react";
+
 export default function ContactMe() {
+  const [showContact, setShowContact] = useState(false);
+
+  const toggleContact = () => {
+    setShowContact(!showContact);
+  };
+
   return (
     <section id="Contact" className="contact--section">
-  <div>
-    <h2>Kontakt</h2>
-    <p className="text-lg"></p>
-  </div>
-  <div className="contact--info">
-  <p><strong>Telefon:</strong> +49 163 630 2259</p>
-    <p><strong>E-Mail:</strong> benedikt.stoeck@web.de </p>
-   
-  </div>
-</section>
+      <div>
+        <p className="text-lg"></p>
+      </div>
+      <div
+        className="contact--info"
+        style={{ display: showContact ? "block" : "none" }}
+      >
+        <p>
+          <strong>Telefon:</strong> +49 163 630 2259
+        </p>
+        <p>
+          <strong>E-Mail:</strong> benedikt.stoeck@web.de{" "}
+        </p>
+      </div>
+      <button onClick={toggleContact} className="btn btn-primary">
+        {showContact ? "Kontaktdaten ausblenden" : "Kontaktdaten anzeigen"}
+      </button>
+    </section>
   );
 }
